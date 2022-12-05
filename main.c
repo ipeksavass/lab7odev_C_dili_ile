24889c5814e8dc318121082e2d2a8885d5858821
ipeksavass <115989338+ipeksavass@users.noreply.github.com>
2022-12-05 22:38:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int x,y,toplam;
+      printf("iki sayi giriniz :");
+      scanf("%d",&x);
+      scanf("%d",&y);
+      toplam=x+y;
+      printf("%d",toplam);
+      return 0;
+}
